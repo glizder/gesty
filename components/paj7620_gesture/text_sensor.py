1e76e5bd858d497291311e0fbafe0b0cf3c2814b
glizder <82045336+glizder@users.noreply.github.com>
2025-06-15 22:57:30 +0200

    Update text_sensor.py

--- components/paj7620_gesture/text_sensor.py
+++ components/paj7620_gesture/text_sensor.py
@@ -7,7 +7,7 @@ from . import paj7620_gesture_ns, PAJ7620GestureComponent
 
 DEPENDENCIES = ["paj7620_gesture"]
 
-CONFIG_SCHEMA = text_sensor.text_sensor_schema.extend(
+CONFIG_SCHEMA = text_sensor.text_sensor_schema(
     {
         cv.GenerateID(): cv.declare_id(PAJ7620GestureComponent),
     }
