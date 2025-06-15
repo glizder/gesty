23057e7b24c77af00d8d9c8f5ec8acac9a4a107b
glizder <82045336+glizder@users.noreply.github.com>
2025-06-15 22:47:36 +0200

    po czascie __init__.py

--- components/paj7620_gesture/__init__.py
+++ components/paj7620_gesture/__init__.py
@@ -1,41 +1,35 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, text_sensor
-from esphome.const import CONF_ID, CONF_ADDRESS, CONF_UPDATE_INTERVAL, CONF_NAME
+from esphome.const import CONF_ID
 
-# Tworzymy przestrzeń nazw dla naszego komponentu w ESPHome
-paj7620_gesture_ns = cg.esphome_ns.namespace("paj7620_gesture")
-PAJ7620GestureSensor = paj7620_gesture_ns.class_(
-    "PAJ7620GestureSensor",
-    text_sensor.TextSensor,
-    cg.PollingComponent,
-    i2c.I2CDevice
-)
-
-# Właściciele kodu (opcjonalne, ale dobra praktyka)
-CODEOWNERS = ["@glizder"]
-
-# Zależności komponentu - w tym przypadku potrzebujemy komponentu I2C
+CODEOWNERS = ["@twoj_github"]
 DEPENDENCIES = ["i2c"]
 
-# Ten schemat jest teraz dla samego komponentu 'paj7620_gesture'
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(PAJ7620GestureSensor),
-        cv.Required(CONF_ID): cv.declare_id(PAJ7620GestureSensor),
-        cv.Required(CONF_NAME): cv.string,
-        cv.Optional(CONF_ADDRESS, default=0x73): cv.i2c_address,
-        cv.Optional(CONF_UPDATE_INTERVAL, default="50ms"): cv.positive_time_period_milliseconds,
-    }
-).extend(i2c.i2c_device_schema(0x73))
+paj7620_gesture_ns = cg.esphome_ns.namespace("paj7620_gesture")
+PAJ7620GestureComponent = paj7620_gesture_ns.class_(
+    "PAJ7620GestureComponent", cg.PollingComponent, i2c.I2CDevice
+)
 
+CONF_GESTURE = "gesture"
 
-# Ta funkcja 'to_code' jest wywoływana, gdy parser ESPHome napotka sekcję 'paj7620_gesture:' w YAML.
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(PAJ7620GestureComponent),
+            cv.Optional(CONF_GESTURE): text_sensor.text_sensor_schema,
+            cv.Optional("address", default=0x73): cv.i2c_address,
+            cv.Optional("update_interval", default="100ms"): cv.update_interval,
+        }
+    )
+    .extend(i2c.i2c_device_schema(False))
+    .extend(cv.polling_component_schema("100ms"))
+)
+
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-
-    await cg.register_component(var, config)
+    var = await cg.register_component(config, PAJ7620GestureComponent.new())
     await i2c.register_i2c_device(var, config)
-
-    cg.add(var.set_name(config[CONF_NAME]))
-    await text_sensor.register_text_sensor(sens, config[CONF_GESTURE])
+    if CONF_GESTURE in config:
+        sens = await text_sensor.new_text_sensor(config[CONF_GESTURE])
+        cg.add(var.set_gesture_sensor(sens))
+        await text_sensor.register_text_sensor(sens, config[CONF_GESTURE])
