82b982d6b2a110cb4e0b348b3fb9e558a15e2f21
glizder <82045336+glizder@users.noreply.github.com>
2025-06-15 23:01:35 +0200

    Update text_sensor.py

--- components/paj7620_gesture/text_sensor.py
+++ components/paj7620_gesture/text_sensor.py
@@ -5,8 +5,6 @@ from esphome.const import CONF_ID
 
 from . import paj7620_gesture_ns, PAJ7620GestureComponent
 
-DEPENDENCIES = ["paj7620_gesture"]
-
 CONFIG_SCHEMA = text_sensor.text_sensor_schema(
     {
         cv.GenerateID(): cv.declare_id(PAJ7620GestureComponent),
